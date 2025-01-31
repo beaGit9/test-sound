b3c2459b91d9b84dc9748002a549b1b2d6b6eed6
beaGit9 <beatriz.calvo@gmail.com>
2025-01-31 19:14:43 +0000

    primer commit sound

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+def button_click(col):
+    speechContainer.audio(speechArr[col], format='audio/mpeg')
+
+st.title("🎈 Aplicación de sonidos")
+st.subheader(
+    "Pulsa encima de la foto para oir a la persona"
 )
+
+names = ['Martin Luther King', ' Kamala Harris', 'Brown']
+imgArr = ['assets/mlk.jpg', 'assets/kamalaharris.png', 'assets/brownjackson.jpg']
+speechArr = ['assets/mlk.mp3', 'assets/kamala.mp3', 'assets/brownjackson.mp3']
+
+cols = st.columns(3)
+
+for c in range(0, len(names)):
+    cols[c].image(imgArr[c])
+    cols[c].button(names[c],on_click=button_click, args=(c,), use_container_width=True)
+
+speechContainer=st.container(border=True)
+speechContainer.write ('Sonido: ')
