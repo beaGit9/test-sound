ba1b0d4a9d438b28d8f45cd9c9d8627c02d57a2d
beaGit9 <beatriz.calvo@gmail.com>
2025-01-31 19:25:50 +0000

    commit 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ for c in range(0, len(names)):
     cols[c].image(imgArr[c])
     cols[c].button(names[c],on_click=button_click, args=(c,), use_container_width=True)
 
-speechContainer=st.container(border=True)
+speechContainer = st.container(border=True)
 speechContainer.write ('Sonido: ')
